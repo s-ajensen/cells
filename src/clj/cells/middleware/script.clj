2db8494b5d4ef1146af48a3f5b328f213e07ade1
g-jensen <greg.micah.jensen@gmail.com>
2025-02-24 22:37:02 -0500

    apply-scripts can take in arbitrary scripts

--- src/clj/cells/middleware/script.clj
+++ src/clj/cells/middleware/script.clj
@@ -1,12 +1,15 @@
 (ns cells.middleware.script
   (:require [cask.core :as cask]))
 
-(defn apply-scripts [state [id {:keys [scripts] :as entity}]]
-  (reduce (fn [state {:keys [scope next-state]}]
-            (case scope
-              :self (assoc-in state [:entities id] (next-state entity))
-              :* (next-state state entity)))
-          state scripts))
+(defn apply-scripts
+  ([state [_id {:keys [scripts] :as entity}]]
+   (apply-scripts state scripts entity))
+  ([state scripts {:keys [id] :as entity}]
+   (reduce (fn [state {:keys [scope next-state]}]
+             (case scope
+               :self (assoc-in state [:entities id] (next-state entity))
+               :* (next-state state entity)))
+           state scripts)))
 
 (deftype ScriptMiddleware []
   cask/Steppable
