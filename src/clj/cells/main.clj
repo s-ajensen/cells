abba077102a78f6532f9823bca291dcda388ef1f
g-jensen <greg.micah.jensen@gmail.com>
2025-02-26 23:21:14 -0500

    fix main

--- src/clj/cells/main.clj
+++ src/clj/cells/main.clj
@@ -1,21 +1,21 @@
 (ns cells.main
   (:require [cask.core :as cask]
-            [cells.engine :as engine]
             [cells.render :as render]
+            [cells.state.window :as window]
             [clojure2d.core :as c2d])
   (:import (cells.engine CellEngine)))
 
 (def window (c2d/show-window
-              {:canvas      (c2d/canvas engine/w engine/h)
+              {:canvas      (c2d/canvas window/w window/h)
                :window-name "Game Window"
-               :w           engine/w
-               :h           engine/h
+               :w           window/w
+               :h           window/h
                :refresher   :onrepaint
                }))
 
 (defn -main [& args]
   (render/init! window)
-  (let [window {:renderer (render/->C2DRenderer window)
+  (let [window {:renderer     (render/->C2DRenderer window)
                 :event-poller (render/->C2DPoller window)}
         engine (CellEngine. window)]
     (cask/game-loop engine 17)
