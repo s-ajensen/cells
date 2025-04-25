172761c4ed3ae84467d81c568efd35c3e4143cd3
g-jensen <greg.micah.jensen@gmail.com>
2025-04-24 20:06:07 -0400

    CellEngine next-state specs

--- spec/clj/cells/engine_spec.clj
+++ spec/clj/cells/engine_spec.clj
@@ -1,13 +1,39 @@
 (ns cells.engine-spec
   (:require [cask.core :as cask]
             [cells.spec-helper :as h]
-            [cells.state.main-menu :as main-menu]
             [cells.engine :as sut]
             [speclj.core :refer :all]))
 
+(deftype IncMiddleware []
+  cask/Steppable
+  (next-state [_this state] (inc state)))
+
+(deftype DoubleMiddleware []
+  cask/Steppable
+  (next-state [_this state] (* 2 state)))
+
 (describe "Cells engine"
 
-  #_(it "has main-menu state"
-    (should= main-menu/state (cask/setup (h/->engine))))
+  (context "next-state"
+    (it "reduces no middlewares"
+      (should-be-nil (cask/next-state (sut/->CellEngine nil []) nil))
+      (should= :state (cask/next-state (sut/->CellEngine nil []) :state)))
+
+    (it "reduces a middleware"
+      (should= 1 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)]) 0))
+      (should= 2 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)]) 1))
+      (should= 4 (cask/next-state (sut/->CellEngine nil [(->DoubleMiddleware)]) 2)))
+
+    (it "reduces multiple middlewares"
+      (should= 4 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)
+                                                         (->DoubleMiddleware)]) 1))
+      (should= 3 (cask/next-state (sut/->CellEngine nil [(->DoubleMiddleware)
+                                                         (->IncMiddleware)]) 1))
+      (should= 5 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)
+                                                         (->DoubleMiddleware)
+                                                         (->IncMiddleware)]) 1))
+      (should= 8 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)
+                                                         (->DoubleMiddleware)
+                                                         (->DoubleMiddleware)]) 1))))
 
   )
