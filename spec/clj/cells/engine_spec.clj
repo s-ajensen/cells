8dfa07cd87bb214b764da7b096077c5c01f074d7
g-jensen <greg.micah.jensen@gmail.com>
2025-04-25 17:46:14 -0400

    refactor engine middleware specs

--- spec/clj/cells/engine_spec.clj
+++ spec/clj/cells/engine_spec.clj
@@ -8,15 +8,15 @@
   cask/Steppable
   (next-state [_this state] state))
 
-(deftype IncMiddleware []
+(deftype ArithmeticMiddleware []
   cask/Steppable
-  (setup [_this state] (inc state))
+  (setup [_this state] (dec state))
   (next-state [_this state] (inc state)))
 
-(deftype DoubleMiddleware []
+(deftype MultiplicativeMiddleware []
   cask/Steppable
-  (setup [_this state] (* 2 state))
-  (next-state [_this state] (* 2 state)))
+  (setup [_this state] (quot state 2))
+  (next-state [_this state] (* state 2)))
 
 (describe "Cells engine"
   (with-stubs)
@@ -31,21 +31,21 @@
       (should-be-nil (cask/setup (sut/->CellEngine nil [(->NoSetupMiddleware)]) nil)))
 
     (it "reduces a middleware"
-      (should= 1 (cask/setup (sut/->CellEngine nil [(->IncMiddleware)]) 0))
-      (should= 2 (cask/setup (sut/->CellEngine nil [(->IncMiddleware)]) 1))
-      (should= 4 (cask/setup (sut/->CellEngine nil [(->DoubleMiddleware)]) 2)))
+      (should= 1 (cask/setup (sut/->CellEngine nil [(->ArithmeticMiddleware)]) 2))
+      (should= 2 (cask/setup (sut/->CellEngine nil [(->ArithmeticMiddleware)]) 3))
+      (should= 4 (cask/setup (sut/->CellEngine nil [(->MultiplicativeMiddleware)]) 8)))
 
     (it "reduces multiple middlewares"
-      (should= 4 (cask/setup (sut/->CellEngine nil [(->IncMiddleware)
-                                                    (->DoubleMiddleware)]) 1))
-      (should= 3 (cask/setup (sut/->CellEngine nil [(->DoubleMiddleware)
-                                                    (->IncMiddleware)]) 1))
-      (should= 5 (cask/setup (sut/->CellEngine nil [(->IncMiddleware)
-                                                    (->DoubleMiddleware)
-                                                    (->IncMiddleware)]) 1))
-      (should= 8 (cask/setup (sut/->CellEngine nil [(->IncMiddleware)
-                                                    (->DoubleMiddleware)
-                                                    (->DoubleMiddleware)]) 1))))
+      (should= 4 (cask/setup (sut/->CellEngine nil [(->ArithmeticMiddleware)
+                                                    (->MultiplicativeMiddleware)]) 9))
+      (should= 3 (cask/setup (sut/->CellEngine nil [(->MultiplicativeMiddleware)
+                                                    (->ArithmeticMiddleware)]) 9))
+      (should= 5 (cask/setup (sut/->CellEngine nil [(->ArithmeticMiddleware)
+                                                    (->MultiplicativeMiddleware)
+                                                    (->ArithmeticMiddleware)]) 13))
+      (should= 3 (cask/setup (sut/->CellEngine nil [(->ArithmeticMiddleware)
+                                                    (->MultiplicativeMiddleware)
+                                                    (->MultiplicativeMiddleware)]) 13))))
 
   (context "next-state"
     (it "reduces no middlewares"
@@ -53,21 +53,21 @@
       (should= :state (cask/next-state (sut/->CellEngine nil []) :state)))
 
     (it "reduces a middleware"
-      (should= 1 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)]) 0))
-      (should= 2 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)]) 1))
-      (should= 4 (cask/next-state (sut/->CellEngine nil [(->DoubleMiddleware)]) 2)))
+      (should= 1 (cask/next-state (sut/->CellEngine nil [(->ArithmeticMiddleware)]) 0))
+      (should= 2 (cask/next-state (sut/->CellEngine nil [(->ArithmeticMiddleware)]) 1))
+      (should= 4 (cask/next-state (sut/->CellEngine nil [(->MultiplicativeMiddleware)]) 2)))
 
     (it "reduces multiple middlewares"
-      (should= 4 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)
-                                                         (->DoubleMiddleware)]) 1))
-      (should= 3 (cask/next-state (sut/->CellEngine nil [(->DoubleMiddleware)
-                                                         (->IncMiddleware)]) 1))
-      (should= 5 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)
-                                                         (->DoubleMiddleware)
-                                                         (->IncMiddleware)]) 1))
-      (should= 8 (cask/next-state (sut/->CellEngine nil [(->IncMiddleware)
-                                                         (->DoubleMiddleware)
-                                                         (->DoubleMiddleware)]) 1))))
+      (should= 4 (cask/next-state (sut/->CellEngine nil [(->ArithmeticMiddleware)
+                                                         (->MultiplicativeMiddleware)]) 1))
+      (should= 3 (cask/next-state (sut/->CellEngine nil [(->MultiplicativeMiddleware)
+                                                         (->ArithmeticMiddleware)]) 1))
+      (should= 5 (cask/next-state (sut/->CellEngine nil [(->ArithmeticMiddleware)
+                                                         (->MultiplicativeMiddleware)
+                                                         (->ArithmeticMiddleware)]) 1))
+      (should= 8 (cask/next-state (sut/->CellEngine nil [(->ArithmeticMiddleware)
+                                                         (->MultiplicativeMiddleware)
+                                                         (->MultiplicativeMiddleware)]) 1))))
 
   (it "renders state with window"
     (cask/render (sut/->CellEngine (h/->window []) []) :state)
